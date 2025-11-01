d355a2b2c726ff96da48d51ddbbb156d9e3c1a00
Shashank Kushwaha <kushwahashashank71@gmail.com>
2025-11-02 04:26:32 +0530

    Add files via upload

---
+++ control.js
@@ -0,0 +1,18 @@
+function but(){
+    window.location.href= "control.html";
+}
+function but2(){
+    window.location.href= "control.html";
+}
+function but3(){
+    window.location.href= "signup.html";
+}
+function but4(){
+    window.location.href= "login.html";
+}
+function but5(){
+    window.location.href= "forgotyourpass.html";
+}
+function but6(){
+    window.location.href= "history.html";
+}
